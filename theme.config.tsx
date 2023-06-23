c22654a83dc614b58fea0c1724be7a380262ccfe
raul-martin-dev <raul.martin4bc@gmail.com>
2023-06-23 13:02:12 +0100

    trying logo in bold

--- theme.config.tsx
+++ theme.config.tsx
@@ -25,7 +25,7 @@ const config: DocsThemeConfig = {
       </svg>
     </div>
     <div style={{ margin: "auto auto auto 15%" }}>
-      Preln
+      <b>Preln</b>
     </div>
   </div>,
   project: {
